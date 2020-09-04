2d96b0428e87365db8119b9d3f3e856a2fcd5d18
ashu1217 <66064345+ashu1217@users.noreply.github.com>
2020-09-04 22:57:41 +0530

    Add files via upload

---
+++ css/login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Animak Rescue</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/animalstyle.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<header>
+		<img src="images/animalrescue.png" class="logo" alt="Logo">
+		
+		<nav>
+			<a href="">Home</a>
+			<a href="">Search</a>
+			<a href="">Medical</a>
+			<a href="">Adopt</a>
+			<a href="">Add Animal</a>
+			<a href="">NGO Service</a>
+			<a href="">Report Abuse</a>
+			<a href="">Volunteer</a>
+			<a href="">About Us</a>
+			<button class="logbtn btn"><a id="logbt" href="login.php">Login</a></button>
+			 
+		</nav>
+    </header>
+ </body>
+</html>    		
